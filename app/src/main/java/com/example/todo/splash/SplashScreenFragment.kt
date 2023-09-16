2ada24dd113d5f6ac67c3aa325744c425dd61e20
Panea Bianca <panea.robertabianca@yahoo.com>
2023-09-16 19:03:59 +0300

    Final Commit with SplashScreen

--- app/src/main/java/com/example/todo/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/example/todo/splash/SplashScreenFragment.kt
@@ -1,12 +1,15 @@
 package com.example.todo.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todo.R
-
+import kotlinx.coroutines.delay
 
 
 class SplashScreenFragment : Fragment() {
@@ -17,7 +20,16 @@ class SplashScreenFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
-    }
 
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Delay for 2 seconds and then navigate to the main fragment
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_toDoFragment2)
+        }, 2000) // 2000 milliseconds (2 seconds)
+    }
 
 }
